bed76b2610034239efe5f4d31f7e9b0b1bb2a0c9
VincentChen-567 <guochenc@usc.edu>
2017-03-27 15:56:32 -0700

    add pinyin list and deal with incorrect pinyin

--- extract.py
+++ extract.py
@@ -10,10 +10,17 @@ character_root_dir = '2474/2474/Lcmc/data/character/'
 pinyin_list = []
 character_list = []
 
+global pinyin_table
+
+
+def read_all_pinyin():
+    global pinyin_table
+    pinyin_table = set(open('pinyin.txt').read().splitlines())
+
 
 def extract_from_xml(root_dir, l):
     for offset in range(18):
-        # for offset in range(1):
+    # for offset in range(1):
         if offset == 8 or offset == 14 or offset == 16:
             continue
         # file_name = f
@@ -30,6 +37,11 @@ def extract_from_xml(root_dir, l):
             l.append('EOS')
 
 
+read_all_pinyin()
+
+print len(pinyin_table)
+# print pinyin_table
+# exit()
 extract_from_xml(pinyin_root_dir, pinyin_list)
 extract_from_xml(character_root_dir, character_list)
 
@@ -45,9 +57,12 @@ with codecs.open('all_in_one.txt', 'w', 'utf-8') as output_file:
         char_sub = re.sub(u'[^\u4e00-\u9fa5]', '', character_list[idx])
 
         pinyin = [x for x in re.split(ur'\d', pinyin_sub)[:-1] if x != '']
+
         char = list(char_sub)
         if len(pinyin) == 0 or len(pinyin) != len(char):
             continue
+
+        pinyin = [x if x in pinyin_table else 'ERROR ' + x for x in pinyin]
 
         z = zip(pinyin, char)
         output_file.write(' '.join(map(lambda x: x[0] + '/' + x[1], z)) + ' ')
