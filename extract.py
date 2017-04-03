2d8fac150d25898e695a00d20848e58807a7b800
William Lee <lee108@usc.edu>
2017-04-03 15:00:32 -0700

    find sentence and word level accuracy

--- extract.py
+++ extract.py
@@ -33,7 +33,6 @@ def extract_from_xml(root_dir, l):
         for sentence in all_sentence:
             for pinyin_word in sentence.findall('w'):
                 l.append(pinyin_word.text)
-
             l.append('EOS')
 
 
