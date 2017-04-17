1f278b2af2056f0d58329070a455f2671ee9d5bf
VincentChen-567 <guochenc@usc.edu>
2017-04-17 14:57:57 -0700

    translate hanzi to pinyin api

--- data/data_hanzi_to_pinyin.py
+++ data/data_hanzi_to_pinyin.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 from subprocess import PIPE, Popen
 import unicodedata
+import time
 
-DATA_FILE = 'data_content.txt'
+# DATA_FILE = 'data_content.txt'
+DATA_FILE = 'ddd.txt'
 OUTPUT_FILE = 'data_content_pinyin.txt'
 
+start = time.time()
+
+i = 0
 with open(DATA_FILE, 'r') as data_file, open(OUTPUT_FILE, 'w') as output_file:
     for line in data_file:
-        sentence = line.strip()
-        print sentence
-        print type(sentence)
-        process = Popen(['trans', '-show-original', 'n', 'zh-CN:zh-CN', sentence], stdout=PIPE, stderr=PIPE)
+        # process = Popen(['trans', '-show-original', 'n', ':zh-CN', line], stdout=PIPE, stderr=PIPE)
+        process = Popen(['trans', '-show-original-phonetics', 'Y', line], stdout=PIPE, stderr=PIPE)
+        # process = Popen(['trans', '-b', '-show-original', 'zh-CN:zh-CN', line], stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
-        print stdout + 'a'
+        # print stdout
+        # exit()
         words = stdout.split('\n')
         data = (words[1].split('(')[1].split(')')[0].decode('utf-8'))
         data = ''.join((c for c in unicodedata.normalize('NFD', data) if unicodedata.category(c) != 'Mn'))
         output_file.write(data + '\n')
+        # exit()
+        # i += 1
+        # print i
+        # if i >= 100:
+        #     break
+end = time.time()
+print end - start
