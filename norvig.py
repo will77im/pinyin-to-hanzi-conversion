d7be2a68351cccd542eaa8c963fa598d3fb3d7c1
William Lee <lee108@usc.edu>
2017-04-26 13:58:12 -0700

    Add norvig's method of finding typo candidates

---
+++ norvig.py
@@ -0,0 +1,50 @@
+'''
+Credit to Peter Norvig
+'''
+import re
+from collections import Counter
+
+
+class Norvig(object):
+
+    WORDS = {}
+
+    def __init__(self, input_file):
+        self.create_word_dict(input_file)
+
+    def words(self, text):
+        return re.findall(r'\w+', text.lower())
+
+    def create_word_dict(self, input_file):
+        with open(input_file, 'r') as in_file:
+            self.WORDS = Counter(self.words(in_file.read()))
+
+    def P(self, word, N=sum(WORDS.values())):
+        "Probability of `word`."
+        return self.WORDS[word] / N
+
+    def correction(self, word):
+        "Most probable spelling correction for word."
+        return max(self.candidates(word), key=self.P)
+
+    def candidates(self, word):
+        "Generate possible spelling corrections for word."
+        return (self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word)) or [word])
+
+    def known(self, words):
+        "The subset of `words` that appear in the dictionary of WORDS."
+        return set(w for w in words if w in self.WORDS)
+
+    def edits1(self, word):
+        "All edits that are one edit away from `word`."
+        letters    = 'abcdefghijklmnopqrstuvwxyz'
+        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+        deletes    = [L + R[1:]               for L, R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+        inserts    = [L + c + R               for L, R in splits for c in letters]
+        return set(deletes + transposes + replaces + inserts)
+
+    def edits2(self, word):
+        "All edits that are two edits away from `word`."
+        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
